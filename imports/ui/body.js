5d9b99543d6b2828205810bf6c88258abc9aec1a
Bayode Aderinola <bayodesegun@gmail.com>
2017-06-16 06:34:25 +0100

    Add user sign-up and sign-in feature

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,3 +1,23 @@
+import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import './message_list.js';
 import './body.html';
+
+Template.body.helpers({
+  user() {
+    if (Meteor.user()) {
+    	return Meteor.user().emails[0].address;
+    }
+	else {
+		return "guest";
+	}
+  },
+  welcomeMsg() {
+    if (Meteor.user()) {
+    	return 'You can see current messages and add or remove your own messages.';
+    }
+	else {
+		return 'Please sign in or sign up to see current messages and add or remove your own messages.';
+	}
+  },
+});
